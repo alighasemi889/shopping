917ee3a2425f055a0c50a29450bc0f256279dcf9
unknown <alihj4223@gmail.com>
2025-04-15 12:52:20 +0330

    add product page and fix some bugs

---
+++ src/js/loader.js
@@ -0,0 +1,6 @@
+window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+      document.getElementById("loader").style.display = "none";
+      document.getElementById("content").style.display = "block";
+    }, 3000); // بعد از ۳ ثانیه
+  });
