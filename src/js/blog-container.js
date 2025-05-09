067414586323440fb1b08c85901cea989a9a7a6e
unknown <alihj4223@gmail.com>
2025-05-09 21:25:49 +0330

    config swiper for blog-container.js

---
+++ src/js/blog-container.js
@@ -0,0 +1,23 @@
+const blogSwiper = new Swiper('.blog-swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.blog-button-next',
+      prevEl: '.blog-button-prev',
+    },
+    pagination: {
+      el: '.blog-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      320:  { slidesPerView: 1, spaceBetween: 10 },
+      640:  { slidesPerView: 2, spaceBetween: 15 },
+      1024: { slidesPerView: 3, spaceBetween: 20 }
+    },
+    lazy: { loadPrevNext: true },
+  });
