2bd46e0b86f9bcd2e15aeb801663fad0928ef543
alighasemi <alihj4223@gmail.com>
2025-05-10 08:56:22 +0330

    refactor: change the pagination and some properties for swiper slider

--- src/js/product-slider.js
+++ src/js/product-slider.js
@@ -1,35 +1,23 @@
-new Swiper('.newest-products-swiper', {
-    slidesPerView: 'auto',
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: false,
-    autoplay: { delay: 3000, disableOnInteraction: false },
-    navigation: {
-      nextEl: '.newest-products-button-next',
-      prevEl: '.newest-products-button-prev',
-    },
-    pagination: {
-      el: '.newest-products-pagination',
-      clickable: true,
-    },
-    breakpoints: {
-      320:  { slidesPerView: 1, spaceBetween: 0 },
-      // 640:  { slidesPerView: 1, spaceBetween: 10 },
-      1024: { slidesPerView: 4, spaceBetween: 10 },
-      500: {
-        slidesPerView: 1,  // فقط یک اسلاید نمایش داده شود
-        spaceBetween:0
-      }
-    },
-    on: {
-      slideChange: function() {
-        document
-          .querySelectorAll('.newest-products-slide')
-          .forEach(slide => slide.classList.remove('newest-products-slide-active'));
-        const active = this.slides[this.activeIndex];
-        active.classList.add('newest-products-slide-active');
-      }
-    }
-  });
-  // فعال کردن کلاس بزرگ‌شدن روی اولین کارت
-  document.querySelector('.newest-products-slide').classList.add('newest-products-slide-active');
+new Swiper('.custom-swiper-container', {
+  direction: 'horizontal',
+  slidesPerView: 1,
+  spaceBetween: 15,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.custom-swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.custom-swiper-button-next',
+    prevEl: '.custom-swiper-button-prev'
+  },
+  breakpoints: {
+    768: { slidesPerView: 2, spaceBetween: 20 },
+    1024: { slidesPerView: 4, spaceBetween: 25 },
+    1280: { slidesPerView: 6, spaceBetween: 30 }
+  }
+});
